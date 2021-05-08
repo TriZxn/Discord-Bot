f2c89876dfed9fe5a2c5983f26e6543331f6f4bc
TriZxn <72722056+TriZxn@users.noreply.github.com>
2021-05-08 10:52:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
       await message.channel.send('Hello!')
+      print(message.author.name)
       print(message.author.id)
       print('Responded to their hello query')
 
     if message.content.startswith('$price'):
       await message.channel.send('*Price*')
-      print(message.author.id)
+      print(message.author.name)
+      print(message.author.id) 
       print('Responded to their price query')
       
 client.run('ODM5NzE3MTMxNDI3MjUwMTk2.YJNthQ.0UeYamqVWk7ZRfg1W8W53Uqdm2E')
